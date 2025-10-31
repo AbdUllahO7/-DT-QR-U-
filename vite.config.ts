19cceb77dad3568222ad6dd2f6ceeb16ddcaff7f
AbdUllah07 <abdallahalhasan09@gmail.com>
2025-10-31 18:03:10 +0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    https: false, // Disable HTTPS
+    https: false, 
     proxy: {
       '/api': {
         target: 'http://localhost:7001',
