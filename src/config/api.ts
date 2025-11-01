017ccdcbac5b59f50da26150628fc323b8aa9fa0
mertcallik <mertclkdev@gmail.com>
2025-11-01 18:43:12 +0300

    ..

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.DEV ? 'http://localhost:7001/api' : 'http://api.mertcode.com/api',
+  BASE_URL: import.meta.env.DEV ? 'http://localhost:7001/api' : 'https://api.mertcode.com/api',
   ENDPOINTS: {
     AUTH: {
       REGISTER: '/Auth/Register',
